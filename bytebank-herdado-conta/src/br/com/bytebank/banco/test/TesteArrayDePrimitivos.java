59f94b8dde288b2fc51dd3ccb3c64dffca7dd668
GabrielOliveiradeAlmeidaJavaAlura <87781070+GabrielOliveiradeAlmeidaJavaAlura@users.noreply.github.com>
2021-08-15 16:05:34 -0300

    fim do cap 1

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayDePrimitivos.java
@@ -0,0 +1,25 @@
+package br.com.bytebank.banco.test;
+
+public class TesteArrayDePrimitivos {
+	public static void main(String[] args) {
+		
+		int[] idades = new int[10];
+		
+		idades[0]= 10;
+		idades[1]= 20;
+		idades[2]=30 ;
+		idades[3]=40;
+		idades[4]=50;
+	
+		for(int indice=0; indice<=idades.length;indice++) {
+			System.out.println(idades[indice]);
+		}
+	}
+		
+		
+		
+		}
+		
+	
+
+
